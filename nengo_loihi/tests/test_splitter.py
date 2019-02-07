7295fbc75bf0a4dc588ea04dd69733f665eed9a6
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2019-02-07 15:25:06 -0500

    Test PESModulatoryTarget interface
    There was an uncovered line in there.

--- nengo_loihi/tests/test_splitter.py
+++ nengo_loihi/tests/test_splitter.py
@@ -481,3 +481,35 @@ def test_splitnetwork_remove_add():
     networks.add(e, "chip")
     networks.remove(e)
     assert e not in networks.adds
+
+
+def test_pesmodulatorytarget_interface():
+    target = "target"
+    p = PESModulatoryTarget(target)
+
+    t0 = 4
+    e0 = [1.8, 2.4, 3.3]
+    t1 = t0 + 3
+    e1 = [7.2, 2.2, 4.1]
+    e01 = np.array(e0) + np.array(e1)
+
+    p.receive(t0, e0)
+    assert isinstance(p.errors[t0], np.ndarray)
+    assert np.allclose(p.errors[t0], e0)
+
+    p.receive(t0, e1)
+    assert np.allclose(p.errors[t0], e01)
+
+    with pytest.raises(AssertionError):
+        p.receive(t0 - 1, e0)  # time needs to be >= last time
+
+    p.receive(t1, e1)
+    assert np.allclose(p.errors[t1], e1)
+
+    errors = list(p.collect_errors())
+    assert len(errors) == 2
+    assert errors[0][:2] == (target, t0) and np.allclose(errors[0][2], e01)
+    assert errors[1][:2] == (target, t1) and np.allclose(errors[1][2], e1)
+
+    p.clear()
+    assert len(list(p.collect_errors())) == 0
