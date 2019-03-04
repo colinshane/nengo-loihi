8429c64485be7855947ab04f2405a5a81e7266e1
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2019-03-04 13:50:09 -0500

    Do not do emergency shutdown behaviour for chip
    Depending on when the error that triggers the shutdown occurs,
    this can create its own errors by trying to read/write the channels.

--- nengo_loihi/simulator.py
+++ nengo_loihi/simulator.py
@@ -702,24 +702,7 @@ class Simulator:
             raise SimulatorClosed("Simulator cannot run because it is closed.")
 
         self._make_run_steps()
-        try:
-            self._run_steps(steps)
-        except Exception:
-            if "loihi" in self.sims and self.sims["loihi"].use_snips:
-                # Need to write to board, otherwise it will wait indefinitely
-                h2c = self.sims["loihi"].nengo_io_h2c
-                c2h = self.sims["loihi"].nengo_io_c2h
-
-                print(traceback.format_exc())
-                print("\nAttempting to end simulation...")
-
-                for _ in range(steps):
-                    h2c.write(h2c.numElements, [0] * h2c.numElements)
-                    c2h.read(c2h.numElements)
-                self.sims["loihi"].wait_for_completion()
-                self.sims["loihi"].n2board.nxDriver.stopExecution()
-                self.sims["loihi"].n2board.nxDriver.stopDriver()
-            raise
+        self._run_steps(steps)
 
         self._n_steps += steps
         logger.info("Finished running for %d steps", steps)
